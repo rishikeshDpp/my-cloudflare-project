e1ddda9026114380e3f958b6913ead512d5569d5
Rishikesh Dasappa <rishikesh@eximpe.com>
2026-02-23 14:59:49 +0530

    env to main

--- src/App.tsx
+++ src/App.tsx
@@ -17,6 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
+      {import.meta.env.VITE_REACT_APP_ENVIRONMENT}
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
